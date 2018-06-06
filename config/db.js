eaebc73b549d06fc0b6c7666544be95639234f1d
Ivan Pashkulev <ivan.pashkulev@gmail.com>
2018-06-06 14:29:12 +0300

    Changed the mongo connection string to pint to MLab

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-const connectionString = "mongodb://localhost:27017/mongo_playground";
+// const connectionString = "mongodb://localhost:27017/mongo_playground";
+const connectionString = "mongodb://pashkulev:abc123@ds247310.mlab.com:47310/mongo_playground";
 
 module.exports = () => {
     mongoose.connect(connectionString).catch(err => console.log(err));
